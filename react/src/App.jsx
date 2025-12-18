76a2e34cd74712a34d9bd0fa9d0865916fdf3a36
sagun basnet <basnetsagun8@gmail.com>
2025-12-18 13:26:01 +0545

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="flex underline p-[] text-[#ffffff] bg-blue-500">Hello</h1>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ react/src/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <>
+      <h1 className="">This is navbar</h1>
+      <p>asdfasdf asjdfk asdhf</p>
+    </>
+  );
+};
+
+export default Navbar;
